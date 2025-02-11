37836fd682e340ff231b0abcd8a32f834568a682
Yasar Cakir <dev.yasarcakir@gmail.com>
2025-02-11 18:12:12 +0700

    main: Removed test files and added express server code

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,116 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import { createWorker, types } from 'mediasoup';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+import { RtpCodecCapability } from 'mediasoup/node/lib/rtpParametersTypes.js';
+import { Server } from 'socket.io';
+import express from 'express';
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+const app = express();
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-explicit-any
-export async function greeter(name: any) {
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+const io = new Server(8000, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+});
+
+const socket = io.of('/peers');
+
+socket.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.emit('connection-success', socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+socket.on('media-event', (data) => {
+  console.log('Media event received:', data);
+});
+
+const mediaCodecs: RtpCodecCapability[] = [
+  {
+    kind: 'audio',
+    mimeType: 'audio/opus',
+    clockRate: 48000,
+    channels: 2,
+    parameters: {
+      useinbandfec: 1,
+    },
+    rtcpFeedback: [
+      { type: 'transport-cc' },
+      { type: 'ccm', parameter: 'fir' },
+      { type: 'nack' },
+      { type: 'nack', parameter: 'pli' },
+    ],
+  },
+  {
+    kind: 'video',
+    mimeType: 'video/VP8',
+    clockRate: 90000,
+    parameters: {
+      'x-google-start-bitrate': 1000,
+    },
+    rtcpFeedback: [
+      { type: 'transport-cc' },
+      { type: 'ccm', parameter: 'fir' },
+      { type: 'nack' },
+      { type: 'nack', parameter: 'pli' },
+    ],
+  },
+];
+
+let worker: types.Worker;
+let router: types.Router;
+let transport: types.WebRtcTransport;
+
+createWorker().then((w) => {
+  worker = w;
+});
+
+worker
+  ?.createRouter({ mediaCodecs })
+  .then((r) => {
+    router = r;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+const rtpCapabilities = router?.rtpCapabilities;
+
+router
+  ?.createWebRtcTransport({
+    listenIps: [
+      {
+        ip: '127.0.0.1:8002',
+        announcedIp: '127.0.0.1:8001',
+      },
+    ],
+    enableUdp: true,
+    enableTcp: true,
+    preferUdp: true,
+  })
+  .then((t) => {
+    transport = t;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+socket.on('getRouterRtpCapabilities', (data, callback) => {
+  callback(rtpCapabilities);
+  console.log('Router RTP Capabilities:', rtpCapabilities);
+  console.log('Router RTP Capabilities:', data);
+});
+
+app.get('/', (_, res) => {
+  res.json({
+    transport: transport,
+    worker: worker,
+    rtpCapabilities: router?.rtpCapabilities,
+  });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
